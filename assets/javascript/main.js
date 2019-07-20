f26f8163307694d70b3c5b0fc03a656463a7e47e
Vanessa de La Cuetara <23455324+vdelacue@users.noreply.github.com>
2019-07-19 21:02:46 -0400

    added multiple design styles to about and projects added thumnails of projects, debugging particle count canvas on top left of screen

---
+++ assets/javascript/main.js
@@ -0,0 +1,10 @@
+
+var removeParticleBox = function(){
+    var parBox = $("canvas")
+    var attr = parBox.attr('width');
+    if(parBox.attr == "80px"){
+        $(this).hide()
+    }
+};
+
+removeParticleBox();
