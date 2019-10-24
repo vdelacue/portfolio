328f7dad1c71a865db6ab6b9af9441ef5064e5fb
Vanessa de La Cuetara <23455324+vdelacue@users.noreply.github.com>
2019-10-23 23:36:27 -0400

    styling navbar debugging mobile responsiveness

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,27 +1,4 @@
-
-var removeParticleBox = function(){
-    var parBox = $("canvas")
-    var attr = parBox.attr('width');
-    if(parBox.attr == "80px"){
-        $(this).hide()
-    }
-};
-
-removeParticleBox();
-
-// ----------Function to turn light bulb picture on and off ----------------//
-// $( "#bulbOff" ).hover(
-//     function() {
-//       $( this ).append( $( "<span> ***</span>" ) );
-//     }, function() {
-//       $( this ).find( "span:last" ).remove();
-//     }
-//   );
-   
-//   $( "li.fade" ).hover(function() {
-//     $( this ).fadeOut( 100 );
-//     $( this ).fadeIn( 500 );
-//   });
+// Easter Egg to toggle color of light bulb picture once resume is clicked
 
 function activateFilterAnimation() {
     $('#bwBulb').toggleClass('animated');
